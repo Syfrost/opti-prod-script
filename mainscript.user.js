cf4ecaa8c082577234ce94af4b8ce418ade9f586
Cedric G <62912213+Syfrost@users.noreply.github.com>
2025-09-24 11:16:11 +0200

    Bump version to 1.7.0

--- mainscript.user.js
+++ mainscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Script collector auto modif DA
 // @namespace    https://github.com/Syfrost/JustWork-Next-Extension
-// @version      1.6.5
+// @version      1.7.0
 // @description  Charge plusieurs scripts distants
 // @author       Cedric G
 // @connect      prod.cloud-collectorplus.mt.sncf.fr
